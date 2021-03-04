5b9e702f1252b60d790770b6a05e2aa88acea6af
boostless <46201063+boostless@users.noreply.github.com>
2021-03-04 10:13:59 +0200

    Update config.lua

--- boost-discordPresence/config.lua
+++ boost-discordPresence/config.lua
@@ -4,3 +4,7 @@ Config.PlayerCount = 32
 Config.PlayerText = "Players" -- Player text. Example Players 10/32
 Config.ResourceTimer = 5 -- Time in witch resource updates in seconds
 Config.UseESXIdentity = false -- Uses ESX Identity name not steam name
+Config.Buttons = { -- Your buttons
+    {index = 0,name = '%Website%',url = 'https://www.google.com'},
+    {index = 1,name = '%Connect%',url = 'fivem://connect/MyIpAddress:30120'}
+}
