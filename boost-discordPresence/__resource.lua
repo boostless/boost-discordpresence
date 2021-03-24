d599d01ad02c89d3e4ef82c60fa9bbbd7e8de411
boostless <46201063+boostless@users.noreply.github.com>
2021-03-24 08:58:49 +0200

    Update __resource.lua

--- boost-discordPresence/__resource.lua
+++ boost-discordPresence/__resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-author 'Boost#7515'
+author 'Boost#4383'
 description 'Discord rich presence with ESX compatibility'
 version '1.0.4'
 
