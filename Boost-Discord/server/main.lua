2e45685a061cd888086bb063bc327a50e3a37263
boostless <46201063+boostless@users.noreply.github.com>
2021-05-10 09:15:40 +0300

    Update main.lua

--- Boost-Discord/server/main.lua
+++ Boost-Discord/server/main.lua
@@ -7,7 +7,7 @@ AddEventHandler('Boost-Discord:UpdatePresence', function()
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	local data = {
-		['PlayerName'] = GetPlayerName(source),
+		['PlayerName'] = GetPlayerName(_source),
 		['IdentityName'] = xPlayer.getName(),
 		['Job'] = {jobName = xPlayer.getJob().name, jobGrade = xPlayer.getJob().grade_label, jobLabel = xPlayer.getJob().label},
 		['ActivePlayers'] = GetNumPlayerIndices()
